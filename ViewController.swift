46462227217d495af6d3ed8550435489990f0cd8
jhtch <jameshotchkiss@att.net>
2014-11-27 21:32:37 -0500

    Testing

---
+++ ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  WikiTag
+//
+//  Created by James Hotchkiss on 11/26/14.
+//  Copyright (c) 2014 James Hotchkiss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var textField:UITextField!
+    @IBOutlet var webView:UIWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didClickGo(AnyObject) {
+        
+        var text = textField.text
+        
+        var randomURL = "http://en.wikipedia.org/wiki/Special:Random"
+        
+        var url = NSURL(string: randomURL)
+        var request = NSURLRequest(URL: url!)
+        webView.loadRequest(request)
+        
+    }
+
+}
+
